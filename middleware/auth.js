045c29a68f3779559aa9b3534acc7423f25dabb0
daichi-sato-design <daichisato.design@gmail.com>
2022-04-04 13:16:13 +0900

    verifyTokenを修正

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,7 @@ const verifyToken = (req, res, next) => {
     return res.status(403).send('A token is required for authorization');
   }
   try {
-    token = token.replace(/^Beaere\s+/, "");
+    token = token.replace(/^Bearer\s+/, "");
     const decoded = jwt.verify(token, config.TOKEN_KEY);
     req.user = decoded;
   } catch (err) {
